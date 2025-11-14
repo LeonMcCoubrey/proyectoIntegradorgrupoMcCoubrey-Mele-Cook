f054120407bda50410306a8ac7cb06aeacc7603d
fmele-pixel <fmele@udesa.edu.ar>
2025-11-14 12:51:26 -0300

    login hava

---
+++ js/login.js
@@ -0,0 +1,18 @@
+let registroForm = document.querySelector(".logeo")
+let correoElectronico = document.querySelector("#email")
+let contraseña = document.querySelector("#password")
+
+registroForm.addEventListener("submit", function(event){
+  event.preventDefault()
+
+  if (correoElectronico.value == ""){
+    alert("Por favor, complete el campo de correo electrónico");
+  }
+
+  else if (contraseña.value == ""){
+    alert("Por favor, complete el campo de contraseña")
+  }
+  else{
+    registroForm.submit()
+  }
+})  
