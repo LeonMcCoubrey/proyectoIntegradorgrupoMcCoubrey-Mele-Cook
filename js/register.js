bf5c79b94620fabbef0eeb218bf63d9d8da7b4c5
fmele-pixel <fmele@udesa.edu.ar>
2025-11-20 16:11:16 -0300

    java de login register y conecte las categorias a la appi falta modificarlas con el css

---
+++ js/register.js
@@ -0,0 +1,48 @@
+let form = document.querySelector(".registrar");
+
+if (form) {
+  form.addEventListener("submit", function (event) {
+
+    event.preventDefault();
+    let emailInput = form.querySelector("#email");
+    let passwordInput = form.querySelector("#contraseña");
+    let repeatPasswordInput = form.querySelector("#repetircontraseña");
+    let terminosInput = form.Terminos; 
+    let email = emailInput.value;
+    let password = passwordInput.value;
+    let repeatPassword = repeatPasswordInput.value;
+
+    if (email === "") {
+      alert("Ingresá un email, por favor.");
+      return;
+    }
+
+    if (password === "") {
+      alert("No se ingreso ninguna contraseña.");
+      return;
+    }
+
+    if (password.length < 6) {
+      alert("La contraseña debe tener al menos 6 caracteres.");
+      return;
+    }
+
+    if (repeatPassword === "") {
+      alert("escribi la contraseña nuevamente.");
+      return;
+    }
+
+    if (password !== repeatPassword) {
+      alert("tiene que coincidir con la contraseña.");
+      return;
+    }
+
+    if (terminos === false) {
+      alert("no se han aceptado los terminos y condiciones.");
+      return;
+    }
+    
+    form.action = "./login.html";
+    form.submit();
+  });
+}
