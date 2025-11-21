e70c8c4cadbea806d421bae8ee50f3665554d207
LeonMcCoubrey <lmccoubrey@udesa.edu.ar>
2025-11-21 17:24:00 -0300

    arreglo pequena cosa de thomas catalogo.js

--- js/catalogo.js
+++ js/catalogo.js
@@ -4,35 +4,48 @@ let titulo = document.querySelector("#tituloCatalogo");
 let params = new URLSearchParams(location.search);
 let categoria = params.get("category");
 
-let url = categoria
-    ? `https://dummyjson.com/products/category/${categoria}`
-    : "https://dummyjson.com/products?";
+let url = undefined;
+
+if (categoria == null) {
+  url = "https://dummyjson.com/products";
+} else {
+  url = "https://dummyjson.com/products/category/" + categoria;
+}
 
 fetch(url)
-  .then(function (response) {
-    return response.json();
+  .then(function (respuesta) {
+    return respuesta.json();
   })
-  .then(function(data)  {
+  .then(function (data) {
 
-    let productos = data.products || data;
+    let productos;
 
-    titulo.innerText = categoria 
-        ? `Categoría: ${categoria}` 
-        : "Catálogo general";
+    if (data.products != undefined) {
+      productos = data.products;
+    } else {
+      productos = data;
+    }
 
-    productos.forEach(function(prod) {
+    if (categoria == null) {
+      titulo.innerText = "Catálogo general";
+    } else {
+      titulo.innerText = "Categoría: " + categoria;
+    }
+
+    for (let i = 0; i < productos.length; i++) {
+      let prod = productos[i];
+
       catalogo.innerHTML += `
-        <article>
-          <a href="./producto.html?id=${prod.id}">
-            <img src="${prod.thumbnail}">
-            <p>${prod.title}</p>
-            <p>Precio: $${prod.price}</p>
-          </a>
-        </article>
-      `;
-    });
-
+                <article>
+                    <a href="./producto.html?id=${prod.id}">
+                        <img src="${prod.thumbnail}">
+                        <p>${prod.title}</p>
+                        <p>Precio: $${prod.price}</p>
+                    </a>
+                </article>
+            `;
+    }
   })
   .catch(function (error) {
-      console.log("Error:", error);
-    });
+    console.log("Error:", error);
+  });
